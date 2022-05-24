fcc8e8dec470e037c062363d8774447a22b3ad56
esanchep <esanchezp85@gmail.com>
2022-05-24 20:03:46 +0200

    Initial commit

---
+++ src/main/java/com/fun/hackinggame/HackingGameApplication.java
@@ -0,0 +1,13 @@
+package com.fun.hackinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fun/hackinggame/HackingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.hackinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
